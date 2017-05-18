d59a25a914142919cbef78dfef13dd9a21a8b095
Melvin Schultz <melvinschultzdev@gmail.com>
2017-05-18 16:02:13 +0200

    part 4.2: controller

---
+++ src/java/monapp/beans/CourseControler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monapp.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author melvin
+ */
+@Named(value = "courseControler")
+@Dependent
+public class CourseControler {
+
+    /**
+     * Creates a new instance of CourseControler
+     */
+    public CourseControler() {
+    }
+    
+}
