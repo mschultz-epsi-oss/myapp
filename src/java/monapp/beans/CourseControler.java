70dd6693a44cf7ed2cd053304d811a2d35599a91
Melvin Schultz <melvinschultzdev@gmail.com>
2017-05-18 16:04:18 +0200

    part 4.2: controller

--- src/java/monapp/beans/CourseControler.java
+++ src/java/monapp/beans/CourseControler.java
@@ -1,25 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package monapp.beans;
 
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
-/**
- *
- * @author melvin
- */
-@Named(value = "courseControler")
-@Dependent
+@ManagedBean(name = "course")
+@SessionScoped
 public class CourseControler {
 
-    /**
-     * Creates a new instance of CourseControler
-     */
-    public CourseControler() {
+    @EJB
+    CourseManager cm;
+    Course theCourse = new Course();
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Create " + this);
+        if (cm.findCourses().size() == 0) {
+            Course c1 = new Course();
+            c1.setName("Architecture JEE");
+            c1.setHours(60);
+            c1.setDescription("Introduction à JEE.");
+            cm.saveCourse(c1);
+        }
     }
-    
+
+    public List<Course> getCourses() {
+        return cm.findCourses();
+    }
+
+    public Course getTheCourse() {
+        return theCourse;
+    }
+
+    public String show(Integer n) {
+        theCourse = cm.findCourse(n);
+        return "showCourse";
+    }
+
+    public String save() {
+        cm.saveCourse(theCourse);
+        return "showCourse";
+    }
+
+    public String newCourse() {
+        theCourse = new Course();
+        return "editCourse";
+    }
 }
